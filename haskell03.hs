8e3596c98bf59a5694c5e9d746d9ad9a9c0ce251
Giordana Camilla Camargo <49041449+giordanacamargo@users.noreply.github.com>
2021-06-10 14:51:29 +0000

    Just for saving reasons

--- haskell03.hs
+++ haskell03.hs
@@ -40,12 +40,45 @@ selectgt5 x = [x | x<-x , x>5]
 
 -- 8 - Usando list comprehension, defina uma função sumOdds :: [Int] -> Int, que receba uma lista e obtenha o somatório dos valores ímpares, produzindo outra lista. Para auxiliar nesta função, você deverá pesquisar funções pré-definidas em Haskell.
 
---sumOdds :: [Int] -> Int
---sumOdds y = foldl (\x y -> odd x) [y]
+sumOdds :: [Int] -> Int
+sumOdds xs = sum [x | x<-xs , odd x]
+
 
 -- 9 - Resolva a questão anterior sem usar list comprehension, criando a função sumOdds' :: [Int] -> Int.
+
+sumOdds' :: [Int] -> Int
+sumOdds' x = sum (filter odd x)
+
 
 -- 10 - Usando list comprehension, defina uma função selectExpr :: [Int] -> [Int], que receba uma lista e selecione somente os valores pares entre 20 e 50, inclusive, produzindo outra lista.
 
 selectExpr :: [Int] -> [Int]
 selectExpr y = [x | x <- y, even x, x>= 20, x<=50]
+
+-- 11 - Escreva uma função countShorts :: [String] -> Int, que receba uma lista de palavras e retorne a quantidade de palavras dessa lista que possuem menos de 5 caracteres. Use list comprehension.
+
+countShorts :: [String] -> Int
+countShorts xs = length [x | x<- xs, length x<5]
+
+-- 12 - Escreva uma função calcExpr :: [Float] -> [Float], que calcule x^2/2 para cada elemento x da lista de entrada e selecione apenas os resultados que forem maiores que 10. Use list comprehension.
+
+calcExpr :: [Float] -> [Float]
+calcExpr xs = [x| x<-xs, ((x^2)/2)>10]
+
+
+-- 13 - Escreva uma função trSpaces :: String -> String, que receba uma string e converta espaços (' ') em traços ('-'). Use list comprehension.
+
+trSpaces :: String -> String
+trSpaces xs = [if x == ' ' then '-' else x | x<-xs]
+
+-- 14 - Usando list comprehension, Defina uma função selectSnd :: [(Int,Int)] -> [Int], que receba uma lista de tuplas e selecione somente os segundos elementos dessas tuplas, produzindo outra lista.
+
+selectSnd :: [(Int,Int)] -> [Int]
+selectSnd xs = [snd x | x<-xs]
+
+-- 15 - Usando a função zip com list comprehension e outras funções auxiliares, escreva uma função dotProd :: [Int] -> [Int] -> Int que calcule o somatório dos produtos dos pares de elementos de duas listas
+
+--dotProd :: [Int] -> [Int] -> Int
+--dotProd x = sum 
+
+
